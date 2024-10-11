c98535a589115fe273796f09c04c78f0f5afbcfa
omarsamy1995 <202301669@pua.edu.eg>
2024-10-11 18:34:17 +0300

    session2

---
+++ matriales/Data type/code/code_1.py
@@ -0,0 +1,20 @@
+input_list= ["amit","learning","python","machine learning","data science","deep learning"]
+remove_user = input("enter your string u want to remove: ")
+if remove_user == "amit":
+    input_list.remove("amit")
+    print(input_list)
+elif remove_user == "learning":
+    input_list.remove("learning")
+    print(input_list)
+elif remove_user == "python":
+    input_list.remove("python")
+    print(input_list)
+elif remove_user == "machine learning":
+    input_list.remove("machine learning")
+    print(input_list)
+elif remove_user == "data science":
+    input_list.remove("data science")
+    print(input_list)
+elif remove_user == "deep learning":
+    input_list.remove("deep learning")
+    print(input_list)
---
+++ matriales/Data type/code/code_2.py
@@ -0,0 +1,40 @@
+# key=[{1,"a"},{"b",2},{"c",3}]
+
+# dic= dict(key)
+# print(dic)
+#______________________________________________________________________________________________________________________
+
+
+# x= [1,2,3]*3
+# y= [2,3,4]*[2]
+# z= [2,3,4]+[2,3,4]
+# c= [2,5]*[5,2]
+# print(c)
+#_______________________________________________________________
+
+# x= 20
+# y=15
+# print((x+True)/(4-y*False))
+#____________________________________________________________
+# x= int(input("enter your first number: "))
+# y= int(input("enter your second number: "))
+# addition= x+y
+# multi = x*y
+# div=x/y
+# print(f"addition {x} and {y} = {addition}")
+# print(f"multiplication {x} and {y} = {multi}")
+# print(f"division {x} and {y} = {div}")
+#____________________________________________________________
+# x= float(input("Enter your score: "))
+# if 90<= x <= 100:
+#     print("your degree is A")
+# elif 80<= x <=90 :
+#     print ("your degree is B")
+# elif 70<= x <= 80 :
+#     print("your degree is C")
+# elif 60<= x <=70:
+#     print("your degree is D")
+# else:
+#     print("your degree is F")
+#_____________________________________________________________________
+
